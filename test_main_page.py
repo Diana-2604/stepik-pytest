a9b6d18f8cec782b8e4ccacb8af3b22484c483e1
Diana-2604 <diana8991@yandex.ru>
2024-01-11 10:52:47 +0300

    add locators and login page

--- test_main_page.py
+++ test_main_page.py
@@ -4,11 +4,6 @@ from pages.main_page import MainPage
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def go_to_login_page(browser):
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
-
-
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу

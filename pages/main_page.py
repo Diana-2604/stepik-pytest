931521771d642f51d7ff5c4d8f1a5ee30470ba4f
Diana-2604 <diana8991@yandex.ru>
2024-01-11 11:03:51 +0300

    initialize login page in test file

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,5 +1,6 @@
 # импорт базового класса BasePage
 from .base_page import BasePage
+from .login_page import LoginPage
 from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
--- test_main_page.py
+++ test_main_page.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from pages.main_page import MainPage
+from pages.login_page import LoginPage
 
 link = "http://selenium1py.pythonanywhere.com/"
 
@@ -8,6 +9,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
 
 def test_guest_should_see_login_link(browser):
